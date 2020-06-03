be3f6e6dd7312fd63296b63e88c2a6de4a1f85d0
Realoj <stephenojwang1040@gmail.com>
2020-06-03 10:22:41 +0300

    Updates

---
+++ precache-manifest.3a7fd41fe1f68197292c03d91f273f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16950f170333a4bd7f60bcd301bd6d13",
+    "url": "/wifi-card/index.html"
+  },
+  {
+    "revision": "8a3152b059ab884925a3",
+    "url": "/wifi-card/static/css/main.5013fa78.chunk.css"
+  },
+  {
+    "revision": "b78348e3859f5c338c70",
+    "url": "/wifi-card/static/js/2.9bca3d5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wifi-card/static/js/2.9bca3d5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a3152b059ab884925a3",
+    "url": "/wifi-card/static/js/main.be85ffab.chunk.js"
+  },
+  {
+    "revision": "dc1052cee18a8c8596b8",
+    "url": "/wifi-card/static/js/runtime-main.b7964a4e.js"
+  }
+]);
